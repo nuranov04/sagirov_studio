be8c198b2f95a4415127b9eeb348c828bfc36eba
Artur <artnyr2004@gmail.com>
2022-11-25 21:47:26 +0600

    finish all functionals

--- bot/views.py
+++ bot/views.py
@@ -1,11 +1,11 @@
 from telebot import TeleBot
 from decouple import config
 
-
 from datetime import datetime
 from bot.models import UserBid
 from utils.class_redis import r
 from utils.helper import user
+from utils.fill_in_form import send_user_values
 
 bot = TeleBot(config("TOKEN"))
 
@@ -46,21 +46,33 @@ def get_email(message):
 
 
 def get_phone_number(message):
-    user.phone_number = message.text
-    msg = bot.send_message(chat_id=message.from_user.id,
-                           text="send me you date of birth. Format DD.MM.YYYY")
-    bot.register_next_step_handler(msg, get_date_of_birth)
+    if message.text[1:].isdigit():
+        user.phone_number = message.text
+        msg = bot.send_message(chat_id=message.from_user.id,
+                               text="send me you date of birth. Format DD.MM.YYYY")
+        bot.register_next_step_handler(msg, get_date_of_birth)
+    else:
+        msg = bot.reply_to(message, text="Not correct phone number")
+        bot.register_next_step_handler(msg, get_phone_number)
 
 
 def get_date_of_birth(message):
     try:
+        date_of_birth = datetime.strptime(message.text, "%d.%m.%Y")
         user.date_of_birth = message.text
         UserBid.objects.create(name=user.name,
                                surname=user.surname,
                                email=user.email,
                                phone_number=user.phone_number,
-                               date_of_birth=datetime.strptime(user.date_of_birth, "%d.%m.%Y"))
-        bot.send_message(chat_id=message.from_user.id, text="Done! Send /start for send again")
+                               date_of_birth=date_of_birth)
+        bot.send_message(chat_id=message.from_user.id, text="Successfully! Please, wait :)")
+        path = send_user_values(user_id=message.from_user.id, name=user.name, surname=user.surname, email=user.email,
+                                phone_number=user.phone_number,
+                                date=user.date_of_birth)
+        if not isinstance(path, TypeError):
+            bot.send_photo(chat_id=message.chat.id, photo=open(path, 'rb'))
+        else:
+            raise ValueError
     except ValueError:
         msg = bot.send_message(chat_id=message.from_user.id, text="Not correct format. Try again! Format DD.MM.YYYY")
         bot.register_next_step_handler(msg, get_date_of_birth)
--- utils/fill_in_form.py
+++ utils/fill_in_form.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 from decouple import config
 
 import time
@@ -9,12 +10,9 @@ import datetime
 
 class Form:
     def __init__(self, user_id, name, surname, email, phone_number, date, executable_path):
-
         self.__options = webdriver.ChromeOptions()
-        self.__driver = webdriver.Chrome(
-            executable_path=executable_path,
-            options=self.__options
-        )
+        self.__options.headless = True
+        self.__driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=self.__options)
         self.user_id = user_id
         self.name = name
         self.surname = surname
@@ -23,139 +21,191 @@ class Form:
         self.date = date
 
     def get_window(self, url: str):
+        """
+        Отпрытие окна по url
+        """
         return self.__driver.get(url)
 
     def _find_element(self, by, value: str):
+        """
+        Поиск элемента
+        """
         return self.__driver.find_element(by, value)
 
     def find_element_by_name(self, value):
+        """
+        Поиск элемента по name
+        """
         return self._find_element(by=By.NAME, value=value)
 
     def find_element_by_xpath(self, value):
+        """
+        Поиск элемента по xpath
+        """
         return self._find_element(by=By.XPATH, value=value)
 
     def find_element_by_class_name(self, value):
+        """
+        Поиск элемента по class name
+        """
         return self._find_element(by=By.CLASS_NAME, value=value)
 
     @staticmethod
     def __send_key(element, key):
+        """
+        Отправка ключа в input
+        """
         return element.send_keys(key)
 
     def send_name(self, element):
+        """
+        Отправка имени в input
+        """
         self.__send_key(element, self.name)
 
     def send_surname(self, element):
+        """
+        Отправка фамилии в input
+        """
         self.__send_key(element, self.surname)
 
     def send_email(self, element):
+        """
+        Отправка почты в input
+        """
         self.__send_key(element, self.email)
 
     def send_phone_number(self, element):
+        """
+        Отправка номера телефона в input
+        """
         self.__send_key(element, self.phone_number)
 
     def send_date(self, element):
+        """
+        Отправка даты в input
+        """
         self.__send_key(element, self.date)
 
     @staticmethod
     def clear_input(element):
+        """
+        Метод для очистки input'ов
+        """
         element.clear()
 
     def delete_script(self, script, element):
+        """
+        Удаление аргумента с помощью скрипта из страницы
+        """
         self.__driver.execute_script(script, element)
 
     def save_screenshot(self, path):
+        """
+        Создание скриншота
+        """
         self.__driver.save_screenshot(path)
 
     def quite(self):
+        """
+        Выход из браузера
+        """
         self.__driver.quit()
 
     def close(self):
+        """
+        закрытие вкладки
+        """
         self.__driver.close()
 
 
-def main(name, surname, user_id, email, phone_number, date):
+def send_user_values(name, surname, user_id, email, phone_number, date):
     executable_path = os.path.abspath("chromdriver/chromedriver")
     form = Form(user_id=user_id, name=name, surname=surname, email=email, phone_number=phone_number, date=date,
                 executable_path=executable_path)
 
     try:
         form.get_window(config("URL"))
-        time.sleep(3)
+        time.sleep(1.5)
 
         # получение полей имя и фамилия
         name_input = form.find_element_by_name("name")
-        print('asd')
         surname_input = form.find_element_by_name("lastname")
 
-        time.sleep(1)
+        time.sleep(0.8)
         # Очистить поля на всякий случай
         form.clear_input(name_input)
         form.clear_input(surname_input)
 
         # Отправка ключей
         form.send_name(name_input)
-        time.sleep(1)
+        time.sleep(0.8)
         form.send_surname(surname_input)
 
-        time.sleep(1)
+        time.sleep(0.8)
 
+        # Поиск кнопки "Продолжить"
         continue_btn = form.find_element_by_class_name("b24-form-btn-block")
 
-        time.sleep(1)
+        time.sleep(0.8)
 
+        # Эмуляция нажатия кнопки "Далее"
         continue_btn.click()
 
-        time.sleep(2)
+        time.sleep(1)
 
+        # Поиск полей
         email_input = form.find_element_by_name('email')
         phone_input = form.find_element_by_name("phone")
 
+        # Очистка этих полей (email, phone number)
         form.clear_input(email_input)
         form.clear_input(phone_input)
 
-        time.sleep(1)
+        time.sleep(0.8)
 
+        # Отправка значений в поля email, phone number
         form.send_email(email_input)
         time.sleep(1)
         form.send_phone_number(phone_input)
 
+        # Поиск кнопки "Далее"
         next_btn = form.find_element_by_xpath(
             "/html/body/main/div/section/div/div/div/div/div/div/div/div/div[2]/form/div[3]/div[2]/button")
 
+        # Эмуляция нажатия кнопки "Далее"
         next_btn.click()
         time.sleep(1)
 
+        # Поиск календаря
         calendar = form.find_element_by_class_name("b24-form-control")
+
         script = "arguments[0].removeAttribute('readonly','readonly')"
+        # Удаление аргумента "readonly" из страницы
         form.delete_script(script=script, element=calendar)
 
+        # Очистка input'а
         calendar.clear()
-        time.sleep(1)
+        time.sleep(0.8)
 
         calendar.send_keys(date)
 
         send_btn_xpath = "/html/body/main/div/section/div/div/div/div/div/div/div/div/div[2]/form/div[4]/div[2]/button"
         send_btn = form.find_element_by_xpath(send_btn_xpath)
-        time.sleep(1)
+        time.sleep(0.8)
 
         send_btn.click()
-
-        time.sleep(3)
+        time.sleep(2)
 
         path = f"{os.path.abspath('screenshots')}/{datetime.datetime.now().strftime('%Y-%m-%d__%H')}:mm__{user_id}.png"
 
+        # сохранение скриншота
         form.save_screenshot(path)
 
         return path
 
     except Exception as ex:
-        print(ex)
         return ex
     finally:
         form.close()
         form.quite()
-
-
-# if __name__ == "__main__":
-#     main(name="Artur", surname='Nuranov', email="adsa@gmail.com", phone_number="+996 (778) 919-715",
-#          date="29.10.2004", user_id="14")
